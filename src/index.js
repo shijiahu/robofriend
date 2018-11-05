c4374c23609ee64ac9a46cc608b1a8221553d38e
Shijia Hu <shijiahu2016a@gmail.com>
2018-11-04 17:26:25 -0800

    add logger in redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider, connect } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import { createLogger } from 'redux-logger';
+import thunkMiddleware from 'redux-thunk';
 import './index.css';
 import App from './containers/App';
 import * as serviceWorker from './serviceWorker';
 import 'tachyons';
 import { searchRobots } from './reducers.js';
 
-const store = createStore(searchRobots);
+const logger = createLogger();
+const store = createStore(searchRobots, applyMiddleware( thunkMiddleware ,logger));
+// 														order: first thunk, then logger
 
 
 ReactDOM.render(
